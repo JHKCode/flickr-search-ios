4adedcd8a361f7c8e7efc830c852549c1bf2be4e
Jinhong Kim <jhkcode@gmail.com>
2017-07-15 15:11:56 +0900

    Setup project

---
+++ FlickrSearch/Sources/FlickrViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FlickrViewController.swift
+//  FlickrSearch
+//
+//  Created by Jinhong Kim on 7/15/17.
+//  Copyright © 2017 Jinhong Kim. All rights reserved.
+//
+
+import UIKit
+
+class FlickrViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Flickr"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
